a47f22df7d2419f58dab566973be32443ba6ee81
John Nolette <john@neetgroup.net>
2017-12-30 15:15:00 -0500

    Finished core es6 migration

--- src/core.js
+++ src/core.js
@@ -10,7 +10,6 @@ export default class {
   constructor(opts) {
     opts = Object.assign({}, opts)
     opts.count = opts.count || 1
-    opts.units = this._plurarize(opts.units.toLowerCase()) || 'sentences'
     opts.sentenceLowerBound = opts.sentenceLowerBound || 5
     opts.sentenceUpperBound = opts.sentenceUpperBound || 15
     opts.paragraphLowerBound = opts.paragraphLowerBound || 3
@@ -20,15 +19,6 @@ export default class {
     opts.suffix = opts.suffix
 
     this.opts = opts
-  }
-
-  /**
-   * Simple method to plurarize words.
-   * @param {string} str - Word to plurarize.
-   * @returns {string}
-   */
-  _plurarize(str) {
-    return str.endsWith('s') ? str : `${str}s`
   }
 
   /**
@@ -96,83 +86,4 @@ export default class {
     return paragraph
   }
 
-
 }
-
-function generator() {
-
-  function randomParagraph(words, lowerBound, upperBound, sentenceLowerBound, sentenceUpperBound) {
-    var paragraph = ''
-      , bounds = {min: 0, max: randomInteger(lowerBound, upperBound)};
-
-    while (bounds.min < bounds.max) {
-      paragraph += '. ' + randomSentence(words, sentenceLowerBound, sentenceUpperBound);
-      bounds.min++;
-    }
-
-    if (paragraph.length) {
-      paragraph = paragraph.slice(2);
-      paragraph += '.';
-    }
-
-    return paragraph;
-  }
-
-  var iter = 0
-    , bounds = {min: 0, max: count}
-    , string = ''
-    , prefix = ''
-    , openingTag
-    , closingTag;
-
-  if (format === 'html') {
-    openingTag = '<p>';
-    closingTag = '</p>';
-  }
-
-  while (bounds.min < bounds.max) {
-    switch (units.toLowerCase()) {
-      case 'words':
-        string += ' ' + randomWord(words);
-        break;
-      case 'sentences':
-        string += '. ' + randomSentence(words, sentenceLowerBound, sentenceUpperBound);
-        break;
-      case 'paragraphs':
-        var nextString = randomParagraph(words, paragraphLowerBound, paragraphUpperBound, sentenceLowerBound, sentenceUpperBound);
-
-        if (format === 'html') {
-          nextString = openingTag + nextString + closingTag;
-          if (bounds.min < bounds.max - 1) {
-            nextString += suffix; // Each paragraph on a new line
-          }
-        } else if (bounds.min < bounds.max - 1) {
-          nextString += suffix + suffix; // Double-up the EOL character to make distinct paragraphs, like carriage return
-        }
-
-        string += nextString;
-
-        break;
-    }
-
-    bounds.min++;
-  }
-
-  if (string.length) {
-    var pos = 0;
-
-    if (string.indexOf('. ') === 0) {
-      pos = 2;
-    } else if (string.indexOf('.') === 0 || string.indexOf(' ') === 0) {
-      pos = 1;
-    }
-
-    string = string.slice(pos);
-
-    if (units === 'sentences') {
-      string += '.';
-    }
-  }
-
-  return string;
-};
