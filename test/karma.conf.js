b903e211c72977fa024875816b3d7bad4de0f975
John Nolette <john@neetgroup.net>
2017-12-30 14:01:12 -0500

    Added karma config

---
+++ test/karma.conf.js
@@ -0,0 +1,26 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    browsers: ['Electron'],
+    frameworks: ['jasmine', 'riot'],
+    files: [
+      '../dist/foobar-ipsum.js',
+    ],
+    reporters: ['progress', 'coverage'],
+    preprocessors: {
+      '../dist/foobar-ipsum.js': ['coverage']
+    },
+    coverageReporter: {
+      reporters: [
+        {type : 'html', dir : '../coverage/'},
+        {type:'lcovonly', dir : '../coverage/'},
+        {type:'json', dir : '../coverage/'}
+      ]
+    },
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    singleRun: true
+  })
+}
